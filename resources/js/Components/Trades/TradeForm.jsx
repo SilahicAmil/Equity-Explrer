3a675a040899afdd64893a9f8f3e3c5d0ccd3498
Amil Silahic <jinobie02@gmail.com>
2025-02-10 21:28:08 -0700

    Creating the main layout of the app. Also making dummy trade form component

---
+++ resources/js/Components/Trades/TradeForm.jsx
@@ -0,0 +1,28 @@
+const TradeForm = () => {
+  return (
+    <form action="">
+      <div className="mb-2">
+        <label htmlFor="">Stock Name</label>
+        <input type="text" />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="">Stock Name</label>
+        <input type="text" />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="">Stock Name</label>
+        <input type="text" />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="">Stock Name</label>
+        <input type="text" />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="">Stock Name</label>
+        <input type="text" />
+      </div>
+    </form>
+  );
+};
+
+export default TradeForm;
